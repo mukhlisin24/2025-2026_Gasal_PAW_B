51907a544a9c74901e2c0603c37bd2035027509d
AinurRaftuzzaki <zakipheonix@gmail.com>
2025-09-26 19:28:33 +0700

    Modul 1 selesai

---
+++ Modul 1/24-034_Ainur_Raftuzzaki/5.php
@@ -0,0 +1,11 @@
+<?php
+$nama = "Zaki";
+$umur = 20;
+$IPK = 3.5;
+$Mahasiswa = true;
+
+echo "Nama: $nama <br>";
+echo "Umur: $umur <br>";
+echo "IPK: $IPK <br>";
+echo "Mahasiswa: " . ($Mahasiswa ? "Ya" : "Tidak");
+?>
