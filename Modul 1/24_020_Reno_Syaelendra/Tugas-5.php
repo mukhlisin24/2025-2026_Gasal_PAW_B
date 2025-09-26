1d1032cd04e25898adcd07ef1cf5e642d1700b70
Reno Syaelendra <renoslendra@gmail.com>
2025-09-26 21:07:44 +0700

    Tugas praktikum modul 1 sudah selesai

---
+++ Modul 1/24_020_Reno_Syaelendra/Tugas-5.php
@@ -0,0 +1,11 @@
+ <?php
+$nama = "Renos";
+$umur = 20;
+$tinggi = 1.75;
+$aktif = true;
+
+echo "Nama: $nama<br>";
+echo "Umur: $umur<br>";
+echo "Tinggi: $tinggi meter<br>";
+echo "Status Aktif: " . ($aktif ? "Ya" : "Tidak");
+?>
