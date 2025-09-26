8dc9a91ddb8b2ab61be2a4d6dd1fa438b5cfa0c6
Pengk0 <roqysofyan@gmail.com>
2025-09-26 23:30:37 +0700

    Menyelesaikan Tugas

---
+++ Modul 1/23-205_Roky_Zofyan_Amirullah/TP3.php
@@ -0,0 +1,12 @@
+<?php
+// Komentar untuk 1 baris (single-line)
+
+# Ini komentar 1 baris juga
+
+/*
+Ini adalah
+komentar multi-baris.
+*/
+
+echo "ini adalah komentar.";
+?>
--- Modul 1/TP1.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Menampilkan teks sederhana
-echo "Hello, World!";
-echo "<br>"; // Tag <br> untuk pindah baris di HTML
-echo "Saya sedang belajar Dasar - dasar PHP .";
-?>
