6a1eccb1967124b5eda9f585a17c2ee4f8199cac
Ari <aringga098@.gmailcom>
2025-09-26 21:50:09 +0700

    Menambahkan Tugas Modul 1 - 24-000_NamaKamu

---
+++ Modul 1/23-104_Moch_Sigit_Aringga/nomor1.php
@@ -0,0 +1,9 @@
+<?php
+
+echo 'nomor1 dan ini adalah tugas praktikum pertama';
+echo "<br>";
+echo "saya sedang mengerjakan tugas praktikum pertama saya dan ini nomor 1";
+echo "<br>";
+echo 'ini ditampilkan menggunakan echo'
+
+?>
---
+++ Modul 1/23-104_Moch_Sigit_Aringga/nomor4.php
@@ -0,0 +1,15 @@
+<?php
+
+// Variabel PHP selalu diawali tanda dolar ($)
+$color = "biru";
+$COLOR = "ungu";
+$coLOR = "oren";
+
+echo 'ini adalah soal nomor 4';
+echo '<br>';
+echo '<br>';
+echo "Warna 1 adalah " . $color . "<br>";
+echo "Warna 2 adalah " . $COLOR . "<br>";
+echo "Warna 3 adalah " . $coLOR . "<br>";
+
+?>
