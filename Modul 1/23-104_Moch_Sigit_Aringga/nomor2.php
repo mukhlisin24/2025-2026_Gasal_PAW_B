400e31dd43451cfbf6502821f7da423d2c2a26bc
Ari <aringga098@.gmailcom>
2025-09-26 21:51:42 +0700

    tugas 1 selesai

--- Modul 1/23-104_Moch_Sigit_Aringga/nomor2.php
+++ Modul 1/23-104_Moch_Sigit_Aringga/nomor2.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>nomor2</title>
+    <title>nomor 2</title>
 </head>
 <body>
     <h3>halo, saya ssedang mengerjakan tugas praktikum paw pertama dan ini nomor 2</h3>
