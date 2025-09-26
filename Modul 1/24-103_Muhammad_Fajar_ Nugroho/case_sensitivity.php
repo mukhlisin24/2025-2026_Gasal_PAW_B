406c006b379a4adb417dea9c7bcf7d71a0218eb4
Buthzz <mfajarninforutm@gmail.com>
2025-09-26 22:24:58 +0700

    fitur: tambah file latihan dasar PHP untuk tugas pertemuan pertama
    - Biodata mahasiswa (nama, NIM, kelas, prodi, alamat, hobi)
    - Jenis dan sintaks komentar PHP
    - Definisi fungsi dengan berbagai parameter
    - Teknik manipulasi string
    - Operasi aritmatika dasar
    - Interpolasi dan penggabungan string
    - Demonstrasi berbagai tipe data PHP
    - Contoh case sensitivity di PHP
    - Integrasi script PHP dalam HTML
    - Penggunaan pernyataan echo

---
+++ Modul 1/24-103_Muhammad_Fajar_ Nugroho/case_sensitivity.php
@@ -0,0 +1,9 @@
+<?php
+$color = "red";
+echo "$color  merupakan model warna aditif yang menjadi dasar dalam dunia digital.<br>";
+$COLOR = "green";
+echo "$COLOR  merupakan model warna aditif yang menjadi dasar dalam dunia digital.<br>";
+$coLOR = "blue";
+echo "$coLOR  merupakan model warna aditif yang menjadi dasar dalam dunia digital.<br>";
+
+?>
