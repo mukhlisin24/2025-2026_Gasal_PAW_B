3280aac5cb762c1597d2348304a5a8d2c16dd06b
mukhlisin24 <irwandwi712@gmail.com>
2025-09-26 21:20:46 +0700

    Modul 1 selesai

---
+++ Modul 1/24-010_Irwan_Dwi_Mukhlisin/Tugas-5.php
@@ -0,0 +1,10 @@
+<?php
+$nama = 'Irwan Dwi M';
+$umur = 18;
+$tinggi = 158.5;
+$student = true;
+
+echo "Nama: " . $nama . "<br>";
+echo "Umur: " . $umur . "<br>";
+echo "Tinggi: " . $tinggi . "<br>";
+echo "Mahasiswa: " . ($student ? "Ya" : "Tidak");
