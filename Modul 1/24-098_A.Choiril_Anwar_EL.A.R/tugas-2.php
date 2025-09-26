de7645cbd0caf4b2ef31a7b1e2b1f436e7263dd8
4uriel <achoirilanwar01@gmail.com>
2025-09-26 22:11:31 +0700

    Modul 1 selesai

---
+++ Modul 1/24-098_A.Choiril_Anwar_EL.A.R/tugas-2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p><?php echo "penulisan php normal"; ?></p>
+    <p><? echo "penulisan php singkat";?></p>
+    <p><?= "penulisan php echo";?></p>
+</body>
+</html>
