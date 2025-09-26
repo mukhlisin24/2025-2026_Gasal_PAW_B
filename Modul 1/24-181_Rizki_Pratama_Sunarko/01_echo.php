3542ac0dec7fcf8d83df2f8d0208c76ee3d0bcf6
rizkisunarko <rizkypratama16322@gmail.com>
2025-09-26 21:27:05 +0700

    Tambah Tugas Modul 1 - Rizki Pratama Sunarko (24-181)

---
+++ Modul 1/24-181_Rizki_Pratama_Sunarko/01_echo.php
@@ -0,0 +1,6 @@
+<?php
+// 01_echo.php — Menampilkan output dengan echo
+echo "Hello, world!";
+echo "<br>";
+echo "Perkenalkan, nama saya Rizki Pratama Sunarko. Selamat datang di praktikum PHP!";
+?>
---
+++ Modul 1/24-181_Rizki_Pratama_Sunarko/04_case_sensitivity.php
@@ -0,0 +1,9 @@
+<?php
+$color = "merah";
+$COLOR = "biru";
+$coLOR = "hijau";
+
+echo "Isi dari variabel color = " . $color . "<br>";
+echo "Isi dari varibael COLOR = " . $COLOR . "<br>";
+echo "Isi dari variabel coLOR = " . $coLOR . "<br>";
+?>
