39d0663ef5c1db4f38c600ef26efa81eee6c3824
adirestuuu <163704672+adirestuuu@users.noreply.github.com>
2025-09-26 20:24:48 +0700

    Add files via upload

---
+++ Modul 1/23-162_Restu_Ishariyadi_YaQub/Tugas-2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tugas 2</title>
+</head>
+<body>
+    <h1>Tugas 2</h1>
+    <?php
+        $nama = "Restu Ishariyadi Ya'Qub";
+        $nim = "23-162";
+        echo $nama;
+        echo "<br>";
+        echo $nim;
+    ?>
+</body>
+</html>
---
+++ Modul 1/23-162_Restu_Ishariyadi_YaQub/Tugas-5.php
@@ -0,0 +1,6 @@
+<?php
+$nama = "Adi";          //string
+$umur = 54;             //integer
+$tinggi = 5.75;         //float
+$isMahasiswa = true;    //boolean
+?>
