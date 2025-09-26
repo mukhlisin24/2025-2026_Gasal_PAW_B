c09774150b293836a2dbf7e13eb6065802b87d6f
Zakaria Mujur Prasetyo <zakariamujur6@gmail.com>
2025-09-26 21:11:34 +0700

    24-144_Zakaria_Mujur_Prasetyo

---
+++ Modul 1/24-144_Zakaria_Mujur_Prasetyo/soal2_PHP+HTML.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="id">
+<head>
+  <meta charset="utf-8">
+  <title>02 - PHP + HTML</title>
+</head>
+<body>
+  <h1>Contoh Menggabungkan PHP dan HTML</h1>
+  <?php
+    $pesan = "Halo World dari PHP di dalam HTML!";
+    echo "<p>$pesan</p>";
+  ?>
+</body>
+</html>
---
+++ Modul 1/24-144_Zakaria_Mujur_Prasetyo/soal7_Aritmatika.php
@@ -0,0 +1,12 @@
+<?php
+$a = 07;
+$b = 3;
+
+echo "\$a = $a, \$b = $b<br>";
+echo "Penjumlahan: " . ($a + $b) . "<br>";
+echo "Pengurangan: " . ($a - $b) . "<br>";
+echo "Perkalian: " . ($a * $b) . "<br>";
+echo "Pembagian: " . ($a / $b) . "<br>";
+echo "Modulus: " . ($a % $b) . "<br>";
+echo "Pangkat (pow): " . pow($a, $b) . "<br>";
+?>
