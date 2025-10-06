d9125d7e69b070feb7d91359bad89925e6bda2ad
adirestuuu <163704672+adirestuuu@users.noreply.github.com>
2025-10-06 19:45:16 +0700

    Add files via upload

---
+++ Modul 2/23_162_Restu_Ishariyadi_YaQub/Tugas-9.php
@@ -0,0 +1,14 @@
+<?php
+$nilai = (50);
+
+if ($nilai >= 90) {
+    echo "Nilai A";
+} elseif ($nilai >= 80) {
+    echo "Nilai B";
+} elseif ($nilai >= 70) {
+    echo "Nilai C";
+} elseif ($nilai >= 60) {
+    echo "Nilai D";
+} else {
+    echo "Nilai E";
+}
